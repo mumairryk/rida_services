cabd375aeb35eb0bafb3df9f3cb09dd01a6d61f7
NASAR RUSVIN <nasarmobiria@gmail.com>
2023-10-17 12:56:27 +0530

    api

--- app/Models/UserAdress.php
+++ app/Models/UserAdress.php
@@ -32,10 +32,16 @@ class UserAdress extends Model
     public static function get_user_default_address($userid)
     {
 
-        $adress = UserAdress::where(['status' => 1, 'user_id' => $userid])->select('user_address.id', 'street', 'user_address.location', 'user_address.address_type', 'is_default', 'land_mark', 'building', 'latitude', 'longitude','apartment')
+        $adress = UserAdress::where(['status' => 1, 'user_id' => $userid])->select('user_address.id','full_name','user_address.dial_code','phone', 'street', 'user_address.location', 'user_address.address_type', 'is_default', 'land_mark', 'building', 'latitude', 'longitude','apartment','user_address.state_id','user_address.city_id','states.name as state','cities.name as city')
+         ->leftjoin('country', 'country.id', 'user_address.country_id')
+            ->leftjoin('states', 'states.id', 'user_address.state_id')
+            ->leftjoin('cities', 'cities.id', 'user_address.city_id')
             ->where('is_default', 1)->first();
         if (!$adress) {
-            $adress = UserAdress::where(['status' => 1, 'user_id' => $userid])->select('user_address.id', 'street', 'user_address.location', 'user_address.address_type', 'is_default', 'land_mark', 'building', 'latitude', 'longitude','apartment')
+            $adress = UserAdress::where(['status' => 1, 'user_id' => $userid])->select('user_address.id','full_name','user_address.dial_code','phone', 'street', 'user_address.location', 'user_address.address_type', 'is_default', 'land_mark', 'building', 'latitude', 'longitude','apartment','user_address.state_id','user_address.city_id','states.name as state','cities.name as city')
+            ->leftjoin('country', 'country.id', 'user_address.country_id')
+            ->leftjoin('states', 'states.id', 'user_address.state_id')
+            ->leftjoin('cities', 'cities.id', 'user_address.city_id')
                 ->orderBy('id', 'DESC')->first();
         }
         return $adress;
